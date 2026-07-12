c190e2d58869ae02e750ded2862fccf95b240a42
PowerPizza <102456330+PowerPizza@users.noreply.github.com>
2026-07-12 23:22:49 +0530

    Make SMTP connection a single connection.

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ if __name__ == '__main__':
     # Start the api server auto-automatically when file is run using python - ONLY FOR DEV.
     import os
     os.system("fastapi dev --port 8084 --host 0.0.0.0")
+    exit(0)
 
 import jwt
 from fastapi import FastAPI, APIRouter, Request, Depends, HTTPException
